96ac0f1fd66aacf288b4fadc53a40492500e42bf
Jacob okoro <jaycobokoro@gmail.com>
2017-11-22 04:32:57 +0100

    added get involved

---
+++ contact/js/myJquery.js
@@ -0,0 +1,9 @@
+
+    $('[data-toggle="wizard-radio"]').click(function(){
+        wizard = $(this).closest('.wizard-card');
+        wizard.find('[data-toggle="wizard-radio"]').removeClass('active');
+        $(this).addClass('active');
+        $(wizard).find('[type="radio"]').removeAttr('checked');
+        $(this).find('[type="radio"]').attr('checked','true');
+     
+    });
